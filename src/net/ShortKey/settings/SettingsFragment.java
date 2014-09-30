5880deb89327d3fc19eb13b19b7309edc4ae455e
Hani <jhanihashemi@gmail.com>
2014-09-30 19:40:52 +0330

    add email

--- src/net/ShortKey/settings/SettingsFragment.java
+++ src/net/ShortKey/settings/SettingsFragment.java
@@ -106,7 +106,7 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         if (key.equals(getString(R.string.key_contact_us))) {
             final Intent result = new Intent(android.content.Intent.ACTION_SEND);
             result.setType("plain/text");
-            result.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"jhanihashemi@gmail.com"});
+            result.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"ShortKeyApp@gmail.com"});
             result.putExtra(android.content.Intent.EXTRA_SUBJECT, "ShortKey [Feedback]");
             result.putExtra(android.content.Intent.EXTRA_TEXT, "");
 
